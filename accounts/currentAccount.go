92357a4d54fe6578627937713d32cd75ee3fc985
albertoparente <albertoparentefh@gmail.com>
2022-01-14 23:05:51 -0300

    Adjusting

--- accounts/currentAccount.go
+++ accounts/currentAccount.go
@@ -7,6 +7,15 @@ type CurrentAccount struct {
 	Balance       float64
 }
 
+func (c *CurrentAccount) Deposit(depositAmount float64) (string, float64) {
+	if depositAmount > 0 {
+		c.Balance += depositAmount
+		return "Deposit made successfully!", c.Balance
+	} else {
+		return "Deposit amount less than zero!", c.Balance
+	}
+}
+
 func (c *CurrentAccount) ToWithdraw(withdrawValue float64) string {
 	flagWithdraw := withdrawValue > 0 && withdrawValue <= c.Balance
 
@@ -15,15 +24,6 @@ func (c *CurrentAccount) ToWithdraw(withdrawValue float64) string {
 		return "Withdrawal successful!"
 	} else {
 		return "Insufficient funds!"
-	}
-}
-
-func (c *CurrentAccount) Deposit(depositAmount float64) (string, float64) {
-	if depositAmount > 0 {
-		c.Balance += depositAmount
-		return "Deposit made successfully!", c.Balance
-	} else {
-		return "Deposit amount less than zero!", c.Balance
 	}
 }
 
--- main.go
+++ main.go
@@ -2,38 +2,62 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/go-app-bank/accounts"
 )
 
 func main() {
-	accountAlberto := accounts.CurrentAccount{}
-	accountAlberto.Holder = "Alberto Parente"
-	accountAlberto.AgencyNumber = 123
-	accountAlberto.AccountNumber = 123456789
-	accountAlberto.Balance = 500
 
-	fmt.Println(accountAlberto.Balance)
-	fmt.Println(accountAlberto.ToWithdraw(300))
-	fmt.Println(accountAlberto.Balance)
-	fmt.Println(accountAlberto.ToWithdraw(500))
-	fmt.Println(accountAlberto.Balance)
-	fmt.Println(accountAlberto.ToWithdraw(-300))
-	fmt.Println(accountAlberto.Balance)
-
-	fmt.Println(accountAlberto.Balance)
-	status, value := accountAlberto.Deposit(2000)
-	fmt.Println(status, value)
+	accountAlberto := accounts.CurrentAccount{
+		Holder:        "Alberto Parente",
+		AgencyNumber:  987,
+		AccountNumber: 987654321,
+		Balance:       1000,
+	}
 
 	accountJuliana := accounts.CurrentAccount{
 		Holder:        "Juliana",
 		AgencyNumber:  987,
 		AccountNumber: 987654321,
-		Balance:       100,
+		Balance:       1000,
 	}
 
-	status2 := accountAlberto.Transfer(200, &accountJuliana)
+	status := accountAlberto.Transfer(-200, &accountJuliana)
 
-	fmt.Println(status2)
-	fmt.Println(accountJuliana)
+	fmt.Println(status)
 	fmt.Println(accountAlberto)
+	fmt.Println(accountJuliana)
+
+	/*
+		accountAlberto := accounts.CurrentAccount{}
+		accountAlberto.Holder = "Alberto Parente"
+		accountAlberto.AgencyNumber = 123
+		accountAlberto.AccountNumber = 123456789
+		accountAlberto.Balance = 500
+
+		fmt.Println(accountAlberto.Balance)
+		fmt.Println(accountAlberto.ToWithdraw(300))
+		fmt.Println(accountAlberto.Balance)
+		fmt.Println(accountAlberto.ToWithdraw(500))
+		fmt.Println(accountAlberto.Balance)
+		fmt.Println(accountAlberto.ToWithdraw(-300))
+		fmt.Println(accountAlberto.Balance)
+
+		fmt.Println(accountAlberto.Balance)
+		status, value := accountAlberto.Deposit(2000)
+		fmt.Println(status, value)
+
+		accountJuliana := accounts.CurrentAccount{
+			Holder:        "Juliana",
+			AgencyNumber:  987,
+			AccountNumber: 987654321,
+			Balance:       100,
+		}
+
+		status2 := accountAlberto.Transfer(200, &accountJuliana)
+
+		fmt.Println(status2)
+		fmt.Println(accountJuliana)
+		fmt.Println(accountAlberto)
+	*/
 }
